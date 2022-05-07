1f2c9ed8b452055a553481e3d8ececa95c482a08
leo carvalho <lndrcrvlh@gmail.com>
2022-05-07 18:56:51 -0400

    Fixes typo in romaji

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,8 @@
 import { useState, useEffect } from 'react'
+import hiragana from "./components/hiragana.json"
 
 function App() {
-	const hiragana = [
-		{ romanji: 'a', hiragana: 'あ' },
-		{ romanji: 'i', hiragana: 'い' },
-		{ romanji: 'u', hiragana: 'う' },
-		{ romanji: 'e', hiragana: 'え' },
-		{ romanji: 'o', hiragana: 'お' },
-		{ romanji: 'ka', hiragana: 'か' },
-		{ romanji: 'ki', hiragana: 'き' },
-		{ romanji: 'ku', hiragana: 'く' },
-		{ romanji: 'ke', hiragana: 'け' },
-		{ romanji: 'ko', hiragana: 'こ' },
-		{ romanji: 'sa', hiragana: 'さ' },
-		{ romanji: 'shi', hiragana: 'し' },
-		{ romanji: 'su', hiragana: 'す' },
-		{ romanji: 'se', hiragana: 'せ' },
-		{ romanji: 'so', hiragana: 'そ' },
-		{ romanji: 'ta', hiragana: 'た' },
-		{ romanji: 'chi', hiragana: 'ち' },
-		{ romanji: 'tsu', hiragana: 'つ' },
-		{ romanji: 'te', hiragana: 'て' },
-		{ romanji: 'to', hiragana: 'と' },
-		{ romanji: 'na', hiragana: 'な' },
-		{ romanji: 'ni', hiragana: 'に' },
-		{ romanji: 'nu', hiragana: 'ぬ' },
-		{ romanji: 'ne', hiragana: 'ね' },
-		{ romanji: 'no', hiragana: 'の' },
-		{ romanji: 'ha', hiragana: 'は' },
-		{ romanji: 'hi', hiragana: 'ひ' },
-		{ romanji: 'fu', hiragana: 'ふ' },
-		{ romanji: 'he', hiragana: 'へ' },
-		{ romanji: 'ho', hiragana: 'ほ' },
-		{ romanji: 'ma', hiragana: 'ま' },
-		{ romanji: 'mi', hiragana: 'み' },
-		{ romanji: 'mu', hiragana: 'む' },
-		{ romanji: 'me', hiragana: 'め' },
-		{ romanji: 'mo', hiragana: 'も' },
-		{ romanji: 'ya', hiragana: 'や' },
-		{ romanji: 'yu', hiragana: 'ゆ' },
-		{ romanji: 'yo', hiragana: 'よ' },
-		{ romanji: 'ra', hiragana: 'ら' },
-		{ romanji: 'ri', hiragana: 'り' },
-		{ romanji: 'ru', hiragana: 'る' },
-		{ romanji: 're', hiragana: 'れ' },
-		{ romanji: 'ro', hiragana: 'ろ' },
-		{ romanji: 'wa', hiragana: 'わ' },
-		{ romanji: 'wo', hiragana: 'を' },
-		{ romanji: 'n', hiragana: 'ん' }
-	]
+
 
 	const [input, setInput] = useState('')
 	const [current, setCurrent] = useState(0)
@@ -70,7 +24,7 @@ function App() {
 	const handleSubmit = (e) => {
 		e.preventDefault()
 		 
-		if (input.toLowerCase() === hiragana[current].romanji) {
+		if (input.toLowerCase() === hiragana[current].romaji) {
 			setStreak(streak + 1)
 			setMaxStreak(streak + 1 > maxStreak ? streak + 1 : maxStreak)
 			setError(false)
@@ -79,7 +33,7 @@ function App() {
 			localStorage.setItem('maxStreak', streak + 1 > maxStreak ? streak + 1 : maxStreak)
 		} else {
 			const h = hiragana[current].hiragana
-			const r = hiragana[current].romanji
+			const r = hiragana[current].romaji
 			setError(`Wrong! The correct answer for ${h} is ${r}`)
 			setStreak(0)
 			localStorage.setItem('streak', 0)
