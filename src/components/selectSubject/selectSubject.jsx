cb9d6af378a4227b5962301b618cea927f8b2c76
leo carvalho <lndrcrvlh@gmail.com>
2022-05-09 07:29:48 -0400

    Select Subject selects only Katakana

--- src/components/selectSubject/selectSubject.jsx
+++ src/components/selectSubject/selectSubject.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import Katakana from "../katakana/katakana";
+//import Hiragana from "../hiragana/hiragana";
+// import Katakana from "../katakana/katakana";
 
-function SelectSubject() {
+function SelectSubject(props) {
+  const [katakana, setKatakana] = useState(false);
+  const [hiragana, setHiragana] = useState(false);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setKatakana(true);
+  };
+
+  if (katakana){
+      return <Katakana />
+  }
+
   return (
     <div className="min-h-screen bg-slate-800 text-white text-center p-10">
       <h1 className="text-2xl font-bold">What would you like to study?</h1>
-      <button className="katakana border-2 p-2 rounded-lg m-4">Katakana</button>
+      <button className="katakana border-2 p-2 rounded-lg m-4" onClick={handleClick}>Katakana</button>
       <button className="hiragana border-2 p-2 rounded-lg m-4">Hiragana</button>
     </div>
   );
 }
 
 export default SelectSubject;
+
+// <div className="min-h-screen bg-slate-800 text-white text-center p-10">
+// <h1 className="text-2xl font-bold">What would you like to study?</h1>
+// <button className="katakana border-2 p-2 rounded-lg m-4">
+//   Katakana
+// </button>
+// <button className="hiragana border-2 p-2 rounded-lg m-4">
+//   Hiragana
+// </button>
+// </div>
