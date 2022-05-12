3b8aa107447fdefd61f765241d8a287a73752de7
leo carvalho <lndrcrvlh@gmail.com>
2022-05-12 16:48:17 -0400

    removes unused code

--- src/components/selectSubject/selectSubject.jsx
+++ src/components/selectSubject/selectSubject.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Katakana from "../katakana/katakana";
 import Hiragana from "../hiragana/hiragana";
 
-function SelectSubject(props) {
+function SelectSubject() {
   const [katakana, setKatakana] = useState(false);
   const [hiragana, setHiragana] = useState(false);
 
@@ -71,13 +71,3 @@ function SelectSubject(props) {
 }
 
 export default SelectSubject;
-
-// <div className="min-h-screen bg-slate-800 text-white text-center p-10">
-// <h1 className="text-2xl font-bold">What would you like to study?</h1>
-// <button className="katakana border-2 p-2 rounded-lg m-4">
-//   Katakana
-// </button>
-// <button className="hiragana border-2 p-2 rounded-lg m-4">
-//   Hiragana
-// </button>
-// </div>
