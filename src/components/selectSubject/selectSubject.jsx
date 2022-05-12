501dc6caf98d75db73221098602643d1fd30e0d0
leo carvalho <lndrcrvlh@gmail.com>
2022-05-11 21:12:45 -0400

    Adds handling for hiragana

--- src/components/selectSubject/selectSubject.jsx
+++ src/components/selectSubject/selectSubject.jsx
@@ -1,26 +1,44 @@
 import React, { useState } from "react";
 import Katakana from "../katakana/katakana";
-//import Hiragana from "../hiragana/hiragana";
-// import Katakana from "../katakana/katakana";
+import Hiragana from "../hiragana/hiragana";
 
 function SelectSubject(props) {
   const [katakana, setKatakana] = useState(false);
   const [hiragana, setHiragana] = useState(false);
 
-  const handleClick = (e) => {
+  const handleKatakana = (e) => {
     e.preventDefault();
     setKatakana(true);
   };
 
-  if (katakana){
-      return <Katakana />
+  const handleHiragana = (e) => {
+    e.preventDefault();
+    setHiragana(true);
+  };
+
+  if (katakana) {
+    return <Katakana />;
+  }
+
+  if (hiragana) {
+    return <Hiragana />;
   }
 
   return (
     <div className="min-h-screen bg-slate-800 text-white text-center p-10">
       <h1 className="text-2xl font-bold">What would you like to study?</h1>
-      <button className="katakana border-2 p-2 rounded-lg m-4" onClick={handleClick}>Katakana</button>
-      <button className="hiragana border-2 p-2 rounded-lg m-4">Hiragana</button>
+      <button
+        className="katakana border-2 p-2 rounded-lg m-4"
+        onClick={handleKatakana}
+      >
+        Katakana
+      </button>
+      <button
+        className="hiragana border-2 p-2 rounded-lg m-4"
+        onClick={handleHiragana}
+      >
+        Hiragana  
+      </button>
     </div>
   );
 }
