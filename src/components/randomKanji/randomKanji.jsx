dbe9a34cae8bbc9d59c08297c08ba68c53b0f199
leo carvalho <lndrcrvlh@gmail.com>
2022-05-12 17:54:37 -0400

    Gets random index from kanji file, only displays number

--- src/components/randomKanji/randomKanji.jsx
+++ src/components/randomKanji/randomKanji.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import kanji from "./kanji.json";
 
 function randomKanji() {
-  return (
-    <div>randomKanji</div>
-  )
+  const [randomKanji, setKanji] = useState(0);
+
+  const setRandomKanji = () => {
+    const randomIndex = Math.floor(Math.random() * kanji.length);
+    setKanji(randomIndex);
+  };
+
+  useEffect(() => setRandomKanji(), []);
+
+  return <div>{randomKanji}</div>;
 }
 
-export default randomKanji
+export default randomKanji;
