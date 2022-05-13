56cc0849287a1645edc6f8222b727f5ac5ca0a4f
leo carvalho <lndrcrvlh@gmail.com>
2022-05-13 12:16:00 -0400

    Returns fetched data in array

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,7 +1,15 @@
-import {useEffect} from 'react'
+import { useEffect, useState } from "react";
 
 export const useFetch = (url) => {
+  const [data, setData] = useState("");
+
   useEffect(() => {
-      fetch(url).then(x => x.json()).then(y => console.log(y))
-  },[url])
-}
+    fetch(url)
+      .then((x) => x.json())
+      .then((y) => {
+        setData(y);
+        console.log(y);
+      });
+  }, [url]);
+  return [data];
+};
